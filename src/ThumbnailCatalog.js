d682c01bf22f120388159d73769c5ee030e7d739
kohterai <koh.terai@nyu.edu>
2017-07-20 01:49:08 +0900

    Amame photos added

--- src/ThumbnailCatalog.js
+++ src/ThumbnailCatalog.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
 } from 'react-router-dom'
 import Thumbnail from './Thumbnail'
+import ThumbnailLong from './ThumbnailLong'
 import projectList from './ProjectList'
 
 class ThumbnailCatalog extends Component {
@@ -19,7 +20,19 @@ class ThumbnailCatalog extends Component {
 
 
             {projectList.longThumb ? (
-              <h1>LONG THUMB PLEASE</h1>
+              <div className="thumbnailContainer" style={{
+                              display: 'flex',
+                              flexWrap: 'wrap'}}>
+                {projectList.projects.map((project, index) => (
+                  <ThumbnailLong  key={project.title}
+                              title={project.title}
+                              caption={project.caption}
+                              thumbnail1={project.thumbnail1}
+                              thumbnail2={project.thumbnail2}
+                              thumbnail3={project.thumbnail3}
+                              path={project.path}/>
+                ))}
+              </div>
             ) : (
               <div className="thumbnailContainer" style={{
                               display: 'flex',
